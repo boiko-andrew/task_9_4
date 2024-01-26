91c1bbb027c05cf4eb3710a5e1638dfa38814f6c
Andrew Boiko <boiko_andrew@mail.ru>
2024-01-26 19:53:37 +0300

    Delete junk file

--- src/main/resources/getProductsByCustomerName.sql
+++
@@ -1,5 +0,0 @@
-select public.Orders.product_name
-from public.Customers inner join public.Orders
-    on public.Customers.id = public.Orders.customer_id
-where lower(public.Customers.name) LIKE lower(:name)
-order by public.Orders.product_name;
